3e0cd7f60b166248037821591f1d135128d8b541
Sonia068 <soniathakursonia068@gmail.com>
2025-10-06 12:21:10 -0700

    Create BubbleSort2.cpp

---
+++ 3-ARRAYS.cpp/BubbleSort2.cpp
@@ -0,0 +1,55 @@
+// Array Sorting using Bubble Sort :(With swap function )
+#include <iostream>
+using namespace std;
+
+
+void swap(int &a,int &b){
+    int temp=a;
+        a=b;
+        b=temp;
+}
+
+
+void bubbleSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+            swap(arr[j+1],arr[j]);
+        }
+    }
+}
+}
+
+
+int main(){
+    int n;
+    cout<<"Enter the size of the array:";
+    cin>>n;
+    
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"Enter the value"<<i+1<<":";
+        cin>>arr[i];
+    }
+
+    cout<<"\n";
+
+    cout<<"Before Sorting :"<<endl;
+     for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+}
+
+
+    for(int i=0;i<n;i++){
+        bubbleSort(arr,n);
+}
+
+cout<<"\n";
+cout<<"After Sorting :"<<endl;
+for(int i=0;i<n;i++){
+    cout<<arr[i]<<" ";
+}
+
+
+return 0;
+}
