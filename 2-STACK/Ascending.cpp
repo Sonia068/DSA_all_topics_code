0c9c5e5e88cab594063a3d9f2777f3a659c93d52
Sonia Thakur <soniathakursonia068@gmail.com>
2025-10-25 08:08:25 -0700

    Create Ascending.cpp

---
+++ 2-STACK/Ascending.cpp
@@ -0,0 +1,54 @@
+//Construct an algorithm to sort a stack in ascending order using only one extra stack:
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void sortStack(stack<int>& s1) {
+    stack<int> s2; 
+
+    while (!s1.empty()) {
+        int temp = s1.top();
+        s1.pop();
+
+
+        while (!s2.empty() && s2.top() > temp) {
+            s1.push(s2.top());
+            s2.pop();
+        }
+
+        s2.push(temp);
+    }
+
+    while (!s2.empty()) {
+        s1.push(s2.top());
+        s2.pop();
+    }
+
+    
+    cout << "Stack in ascending order (top to bottom): ";
+    while (!s1.empty()) {
+        cout << s1.top() << " ";
+        s1.pop();
+    }
+    cout << endl;
+}
+
+int main() {
+    stack<int> s;
+    int n, num;
+
+    cout << "Enter the number of elements in the stack: ";
+    cin >> n;
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> num;
+        s.push(num);
+    }
+
+    sortStack(s);
+
+    return 0;
+}
+
+
