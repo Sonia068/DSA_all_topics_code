e849d4d2abe343fe9194c658434f3362e2670658
Sonia Thakur <soniathakursonia068@gmail.com>
2026-03-07 01:34:22 -0500

    Add Matrix Weight

---
+++ 11-GRAPH/Representation/MatrixWt.cpp
@@ -0,0 +1,62 @@
+//--------------Adjacency matrix(Graph)--------------(Weighted graph)
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, e;
+    cout << "Enter the number of vertices: ";
+    cin >> n;
+    cout << "Enter the number of edges: ";
+    cin >> e;
+
+    // Edges -> variable ,so matrix  size is determined by number of vertices (n) not edges (e)
+
+    /*
+    // Zero-based indexing in graph 
+
+    // Create an adjacency matrix
+    int graph[n][n];            
+    
+    // Initialize the adjacency matrix with 0s
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            graph[i][j] = 0;
+        }
+    }
+    */
+
+
+    // One-based indexing in graph
+    
+    // Create an adjacency matrix
+    int graph[n+1][n+1];
+
+    // Initialize the adjacency matrix with 0s
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            graph[i][j] = 0;
+        }
+    }
+
+    // Input edges and populate the adjacency matrix
+    cout << "Enter the edges (source destination):" << endl;
+    for (int i = 0; i < e; i++) {
+        int src, dest,wt;
+        cin >> src >> dest>> wt;
+        graph[src][dest] = wt; // Assuming a directed graph
+        graph[dest][src] = wt; // For undirected graphs
+    }
+
+    // Print the adjacency matrix
+    cout << "Adjacency Matrix:" << endl;
+    for (int i = 0; i <=n; i++) {
+        for (int j = 0; j <=n; j++) {
+
+            cout << graph[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
